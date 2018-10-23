83ab56868ad55e7febf0adfdd9523c9d995e7d79
Hasnain Al-taj <37552020+Hasnainaltaj@users.noreply.github.com>
2018-10-23 15:22:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,14 +12,14 @@ public class Main {
 
     public static void merge()throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the size of the arrays: ");
-        int l = Integer.parseInt(br.readLine());
-        int x[] = new int[l];
-        int y[] = new int[l];
-        int z[] = new int[l*2];
-        System.out.println("Enter the Elements for the first array");
-        for (int i = 0; i < l; i++) {
-            x[i]= Integer.parseInt(br.readLine());
+        System.out.println("Enter the size of the arrays: ");   //طلب ادخال حجم الاري
+        int l = Integer.parseInt(br.readLine()); // قرائة الحجم
+        int x[] = new int[l]; // initialize first array
+        int y[] = new int[l]; // initialize second array
+        int z[] = new int[l*2]; // initialize final array
+        System.out.println("Enter the Elements for the first array"); // طلب ادخال العناصر
+        for (int i = 0; i < l; i++) { // بدء اللوب للأدخال العناصر
+            x[i]= Integer.parseInt(br.readLine()); // تحويلها الى ارقام 
         }
         System.out.println("Enter the Elements for the second array");
         for (int i = 0; i < l; i++) {
@@ -34,6 +34,7 @@ public class Main {
             else
                 z[i] = y[k++];
         }
+        // طباعة بعد الدمج
         for (int i =0; i<z.length; i++)
             System.out.print(z[i]+" ");
         System.out.println();
